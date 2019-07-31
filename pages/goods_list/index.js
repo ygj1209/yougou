45feefc988c9090274507a4a9459d41e3048a9d6
yuanguojing <912465127@qq.com>
2019-07-31 23:24:03 +0800

    动态渲染+下拉提示无下页数据

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,4 +1,5 @@
 // pages/goods_list/index.js
+import { request } from "../../request/index.js";
 Page({
 
   /**
@@ -10,6 +11,50 @@ Page({
       {id:1,title:"销量",Tabscurrgoods:false},
       {id:2,title:"价格",Tabscurrgoods:false}
     ],
+    getgoods:[]
+  },
+  QueryParams:{
+    query: "",
+    // 分类id 
+    cid: "",
+    // 页码 
+    pagenum: 1,
+    // 页容量
+    pagesize: 10
+  },
+   // 总页数
+   TotalPages: 1,
+  onLoad(options) {
+    this.QueryParams.cid=options.cid;
+    this.aaaa();
+  },
+ 
+  //商品详情请求
+  aaaa() {
+    request({
+      url:"/goods/search",
+      data:this.QueryParams
+    })
+    .then(res=>{
+      // console.log (res)
+      //判断有无下一页的数据,总页数=总条数/页容量
+      this.TotalPages = Math.ceil(res.total / this.QueryParams.pagesize);
+      this.setData({
+        getgoods:[...this.data.getgoods,...res.goods]
+      })
+    })
+  },
+   //页面滚动到低处触发
+   onReachBottom() {
+    //判断当前总页数是否大于0
+    if(this.QueryParams.pagenum>=this.TotalPages) {
+        // 没有下一页数据
+      console.log("没有下一页数据");
+    } else {
+      this.QueryParams.pagenum++;
+
+      this.aaaa
+    }
   },
   //tab栏切换
   handleTitleChange(e) {
