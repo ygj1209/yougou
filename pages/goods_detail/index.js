db5885c3ac916126f04ed484fe4e7088decb9a58
yuanguojing <912465127@qq.com>
2019-08-02 01:57:30 +0800

    商品详情动态渲染部分

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,66 +1,35 @@
 // pages/goods_detail/index.js
+import { request } from "../../request/index.js";
+import regeneratorRuntime from '../../lib/runtime/runtime';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    goodsObj:{}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+     this.getGoodsDetail(options.goods_id)
 
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+   
+  //获取商品详情
+  async getGoodsDetail(goods_id) {
+      const res=await request({url:"/goods/detail",data:{goods_id}})
+      console.log (res)
+      this.setData({
+        goodsObj:{
+          goods_name:res.goods_name,
+          goods_price:res.goods_price,
+          //把webp的图片格式替换成jpg的格式
+          goods_introduce:res.goods_introduce.replace(/\.webp/g,'.jpg'),
+          pics:res.pics
+        }
+      })
+   }  
 })
