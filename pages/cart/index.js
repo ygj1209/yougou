33abfa311a5b227ef57e19d146fe0ca724d0449a
yuanguojing <912465127@qq.com>
2019-08-02 12:09:52 +0800

    实现收货地址

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -15,52 +15,42 @@ Page({
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+  // 获取收货地址
+  handleChooseAddress(){
 
-  },
+    // 1 获取用户对小程序的授权信息
+    wx.getSetting({
+      success: (result1) => {
+        // 获取到了授权信息
+        const scopeAddress=result1.authSetting['scope.address'];
+        // 用户授权过  或者 用户 从来没有调用过收货地址  
+        if(scopeAddress===true||scopeAddress===undefined){
+          // 1.1 调用收货地址
+          wx.chooseAddress({
+            success: (result2) => {
+              console.log(result2);
+            }
+          });
+        }else{
+          // 2.1用户 点击 拒绝收货地址  诱导用户 打开授权页面 再调用获取收货地址
+          wx.openSetting({
+            success: () => {
+              // 2.2 再去调用 获取收货地址
+              wx.chooseAddress({
+                success: (result3) => {
+                  console.log(result3);
+                },
+                fail: () => {},
+                complete: () => {}
+              });
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+            }
+          });
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+        }
+      },
+      fail: () => {},
+      complete: () => {}
+    });
   }
 })
